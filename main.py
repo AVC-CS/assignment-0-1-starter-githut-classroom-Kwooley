90adc97029a75ff44d1a26bb2207f98a60e72732
Kyu Lee <kwooler@gmail.com>
2023-08-24 13:47:02 -0700

    A01 Done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
-
     ##############################
     # make your code below
     # print('Hello World')
     ##############################
+    print ('Hello World')
+    print ('CS122: Python Programming')
 
 
 if __name__ == '__main__':
